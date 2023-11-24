9c499f731f91c434d4f4d049cc04bc6664243b3d
Alexandra <Alexandra@>
2023-11-24 19:26:02 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab45_KosinovaAi
+    {
+public static class Converter
+{
+        public static int Do(float x)
+        {
+            return int.MinValue;
+        }
+        public static void Main(string[] args)
+        {
+            Do(Do(0.0f));
+        }
+}
+}
